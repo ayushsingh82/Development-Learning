afff6ef60fbafd45418a9d9a6f3f87bb02a9a756
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-02-13 22:54:24 +0530

    transaction

--- Solana/module4.ts
+++ Solana/module4.ts
@@ -1,4 +1,7 @@
 import * as borsh from "@project-serum/borsh"
+import * as web3 from "@solana/web3.js";
+const BN=require('bn.js')
+var a=BN('737498',10)
 
 const equipPlayerSchema=borsh.struct([
     borsh.u8('variant'),
@@ -8,8 +11,31 @@ const equipPlayerSchema=borsh.struct([
 
 const buffer=Buffer.alloc(1000)
 
-// equipPlayerSchema.encode({variant:2,playerId:1435,itemId:7374},buffer)
+equipPlayerSchema.encode({variant:2,playerId:1435,itemId:new BN('737498',10)},buffer)
 
-// const instructionBuffer= buffer.slice(0,equipPlayerSchema.getSpan(buffer))
+const instructionBuffer= buffer.subarray(0,equipPlayerSchema.getSpan(buffer))
 
 console.log(equipPlayerSchema.getSpan(buffer))
+
+const endpoint=web3.clusterApiUrl('devnet')
+const connection=new web3.Connection(endpoint)
+
+const transaction=new web3.Transaction()
+const instruction = new web3.TransactionInstruction({
+    keys: [
+      {
+        pubkey: player.publicKey, // Assuming player is a valid PublicKey object
+        isSigner: true,
+        isWritable: false,
+      },
+      {
+        pubkey: playerInfoAccount, // Assuming playerInfoAccount is a valid PublicKey object
+        isSigner: false,
+        isWritable: true,
+      }
+    ],
+    data:instructionBuffer,
+    programId:PROGRAM_ID
+  });
+  
+  transaction.add(instruction)
